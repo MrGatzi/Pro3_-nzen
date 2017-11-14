47ee5e9ce5b7cfbcf053bc023c88be25ed9a8561
robinfellinger <robinfellinger99@gmail.com>
2017-11-14 12:33:39 +0100

    modified sql file

--- mysql/munzn.sql
+++ mysql/munzn.sql
@@ -3,11 +3,10 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2017 at 06:39 PM
+-- Generation Time: Nov 14, 2017 at 12:31 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
-SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -20,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `munzn`
 --
+CREATE DATABASE IF NOT EXISTS `munzn` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `munzn`;
 
 -- --------------------------------------------------------
 
@@ -27,6 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `charts`
 --
 
+DROP TABLE IF EXISTS `charts`;
 CREATE TABLE `charts` (
   `idcharts` bigint(20) UNSIGNED NOT NULL,
   `user_iduser` bigint(20) UNSIGNED NOT NULL,
@@ -40,6 +42,7 @@ CREATE TABLE `charts` (
 -- Table structure for table `coins`
 --
 
+DROP TABLE IF EXISTS `coins`;
 CREATE TABLE `coins` (
   `idcoins` bigint(20) UNSIGNED NOT NULL,
   `user_iduser` bigint(20) UNSIGNED NOT NULL,
@@ -53,11 +56,12 @@ CREATE TABLE `coins` (
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `iduser` bigint(20) UNSIGNED NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` char(128) NOT NULL,
-  `verified` tinyint(1) NOT NULL
+  `verified` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -65,7 +69,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`iduser`, `email`, `password`, `verified`) VALUES
-(1, 'muster@muster.at', '$2y$10$1ZNdKnw9gZpVBMqdM4PE4eXDmF2BMiqOMfYbNMJ3gY8vOXEe/vePe', 1);
+(1, 'muster@muster.at', '$2y$10$1ZNdKnw9gZpVBMqdM4PE4eXDmF2BMiqOMfYbNMJ3gY8vOXEe/vePe', 1),
+(3, 'robinfellinger99@gmail.com', '$2y$10$kUvC4WNt/F6LCpd7CyrYuObLfWei.ZnzD..48rg8jjjtXagLSYfi2', 1);
 
 --
 -- Indexes for dumped tables
@@ -110,7 +115,7 @@ ALTER TABLE `coins`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `iduser` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `iduser` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
@@ -126,7 +131,6 @@ ALTER TABLE `charts`
 --
 ALTER TABLE `coins`
   ADD CONSTRAINT `coins_ibfk_1` FOREIGN KEY (`user_iduser`) REFERENCES `user` (`iduser`);
-SET FOREIGN_KEY_CHECKS=1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
