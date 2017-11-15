420232bb5687c74fd7bd963dde4c76fe84ba6c7e
MrGatzi <martingatzweiler@gmail.com>
2017-11-15 17:20:08 +0100

    add calculation

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -12,15 +12,21 @@ $(document).ready(function () {
     });
 
     remove();
-
+    $('#calculator').on("change", '.amount', function(){
+        calcUSD();
+    });
+   $('#calculator').on("change", '[name="currency"]', function(){
+        calcUSD();
+    });
 });
+
 
 function crypto() {
     var data1;
 
     $.get("tests/api_que.php", function(data, status){
         data1=JSON.parse(data);
-        console.log(data1);
+       // console.log(data1);
         for (index = 0; index < data1.length; ++index) {
             $('[name="currency"]').append($('<option>', {
                 value: data1[index].price_usd,
@@ -34,5 +40,16 @@ function remove() {
     $('.remove').click(function () {
         var parent = $(this).parent();
         parent.remove();
+        calcUSD();
     });
+    calcUSD();
+
+}
+function calcUSD() {
+    var all=0;
+    $('[name="currency"]').each(function( index ) {
+     all = all +($( this ).val()*$( this ).next().val());
+    });
+    console.log(all)
+    $('.output').text(all);
 }
