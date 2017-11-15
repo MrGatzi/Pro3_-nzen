80a9b063ee44415fcdd67fa55232128ae6edd064
AndreaMaderthaner <andrea.maderthaner@gmail.com>
2017-11-15 14:31:08 +0100

    added v 1.0

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
     $('#login').click(function(){
-        $('#form').removeAttr('class').addClass('log');
+        $('#loginForm').removeAttr('class').addClass('log');
         $('body').addClass('login-active');
     });
 
     $('#close').click(function(){
-        $('#form').addClass('out');
-        $('#form').removeClass('log');
+        $('#loginForm').addClass('out');
+        $('#loginForm').removeClass('log');
         $('body').removeClass('login-active');
     });
 });
