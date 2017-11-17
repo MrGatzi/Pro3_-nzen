6d690d623b23948662a7a9c8921a0c20f9e07844
MrGatzi <martingatzweiler@gmail.com>
2017-11-17 15:33:17 +0100

    small changes to index.twig

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -4,33 +4,50 @@ $(document).ready(function () {
     getcrypto();
     getUSD();
     var code = '\n <div class="newCurrency row">\n <select class="coin" name="currency">\n </select>\n <input class="amount" type="text" name="amount" value="0">\n <button class="button remove" type="button" name="remove">x</button>\n </div>\n';
-    $.get("login/getCoins.php", function(data, status){
-        var port=JSON.parse(data);
-        for (var index = 0; index < port.length; ++index) {
-            var parent = $( "#portfolio" )
+    $.ajax({
+        async: false,
+        type: 'GET',
+        url: 'login/getCoins.php',
+        success: function(data) {
+            var port=JSON.parse(data);
+            for (var index = 0; index < port.length; ++index) {
+                var parent = $( "#portfolio" );
+                var html = $.parseHTML(code);
+                $(html[1]).find('.crypto_coin').val(port[index].currency);
+                $(html[1]).find('.amount').val(port[index].amount);
+                $(html[1]).find('.amount').val(port[index].amount);
+                for (var index2 = 0; index2 < storedValues.length; ++index2) {
+                    $(html[1]).find('.coin').append($("<option></option>")
+                        .attr("value",storedValues[index2].price_usd)
+                        .text(storedValues[index2].symbol)
+                        .prop('selected', function(){
+                            if(storedValues[index2].symbol==port[index].currency){
+                                return true;
+                            }else{
+                                return false;
+                            }
+                        })
+                    );
+                };
+                parent.append(html);
+
+                remove();
+            }
+        },
+        error: function(data) {
+            var parent = $( "#portfolio" );
             var html = $.parseHTML(code);
-            $(html[1]).find('.crypto_coin').val(port[index].currency);
-            $(html[1]).find('.amount').val(port[index].amount);
-            $(html[1]).find('.amount').val(port[index].amount);
             for (var index2 = 0; index2 < storedValues.length; ++index2) {
                 $(html[1]).find('.coin').append($("<option></option>")
                     .attr("value",storedValues[index2].price_usd)
                     .text(storedValues[index2].symbol)
-                    .prop('selected', function(){
-                        if(storedValues[index2].symbol==port[index].currency){
-                            return true;
-                        }else{
-                            return false;
-                        }
-                    })
                 );
             };
             parent.append(html);
-
-            remove();
         }
     });
     $('#add').click(function () {
+        getcrypto();
         var parent = $( "#portfolio" ),
             html = $.parseHTML(code);
         for (var index2 = 0; index2 < storedValues.length; ++index2) {
@@ -50,6 +67,9 @@ $(document).ready(function () {
    $('#calculator').on("change", '[name="currency"]', function(){
         calcUSD();
     });
+    $('#calculator').on("change", '[name="money"]', function(){
+        updateMoney();
+    });
 });
 
 function getcrypto() {
@@ -64,9 +84,6 @@ function getcrypto() {
             return false;
         }
     });
-    /*$.get("tests/api_que.php", function(data, status){
-     storedValues=JSON.parse(data);
-     });*/
     return true;
 }
 
@@ -99,4 +116,13 @@ function getUSD(){
             }));
         });
     });
+}
+function updateMoney(){
+    var all=0;
+    $('[name="currency"]').each(function( index ) {
+        all = all +($( this ).val()*$( this ).next().val());
+    });
+    all=all*$('[name="money"]').val();
+    console.log('hey hey '+$('[name="money"]').val());
+    $('.output').text(all);
 }
