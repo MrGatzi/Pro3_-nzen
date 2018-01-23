8da0e72b0882a5d26eac1460dc9dca49fd88de58
robinfellinger <robinfellinger99@gmail.com>
2018-01-23 09:18:07 +0100

    about

--- about.php
+++ about.php
@@ -6,6 +6,6 @@ require 'vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
-$twig->render('about.twig');
+echo $twig->render('about.twig');
 
 
